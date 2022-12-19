1abe8a287eeb11be9dd99ce313b1b60eccfca716
Matheus Antonio Zucolotto Pereira <matheus.pereira@softplan.com.br>
2022-12-19 13:42:18 -0300

    Adiciona integração airtable

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3030;
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: AIRTABLE_API_KEY
+});
+var base = Airtable.base('app2IBFSfsLFaK4Sq');
 
 app.use(bodyParser.json());
+
+function addEntry(text) {
+    const val = text.split(" ");
+    const day = {fields: {Valor:parseInt(val[0]), Categoria: val[1], Tipo: "Saída"}};
+    base('Day').create([
+        day
+    ], function(err, records) {
+        if (err) {
+            console.error(err);
+        return;
+        }
+        records.forEach(function (record) {
+            console.log(record.getId());
+        });
+    });
+}
 
 app.post('/day-webhook', (req, res) => {
     if(req.body?.type==="url_verification") {
         res.send(req.body?.challenge);
     } else if(req.body?.event?.type==="message") {
         console.log(req.body?.event?.text);
+        addEntry(req.body?.event?.text);
         res.send("");
     } else {
         res.statusCode = 400;
